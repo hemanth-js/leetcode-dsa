5cfad01ff13c7ab801c3c2e0f218710502e2bb8d
HEMANTH J S <hemanthjs1226@gmail.com>
2026-09-13 20:42:02 +0530

    Time: 3 ms (99.62%), Space: 44.2 MB (89.02%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int largestOverlap(int[][] A, int[][] B) {
+        int[] aLeftSlide = makeArray(A), aRightSlide = aLeftSlide.clone(), b = makeArray(B);
+        int maxOverlap = maxOverlapUpDownSlide(aLeftSlide, b);
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A.length; j++) {
+                aLeftSlide[j] <<= 1;
+                aRightSlide[j] >>= 1;
+            }
+            maxOverlap = Math.max(maxOverlap, maxOverlapUpDownSlide(aLeftSlide, b));
+            maxOverlap = Math.max(maxOverlap, maxOverlapUpDownSlide(aRightSlide, b));
+        }
+        return maxOverlap;
+    }
+    private int[] makeArray(int[][] matrix) {
+        int[] array = new int[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1) array[i] |= (1 << j); // set bit
+            }
+        }
+        return array;
+    }
+    private int maxOverlapUpDownSlide(int a[], int[] b) {
+        int maxOverlap = 0;
+        for (int i = 0; i < a.length; i++) {
+            int overlapUp = 0, overlapDown = 0;
+            for (int row = i; row < a.length; row++) {
+                overlapUp += Integer.bitCount(a[row] & b[row - i]);
+                overlapDown += Integer.bitCount(a[row - i] & b[row]);
+            }
+            maxOverlap = Math.max(maxOverlap, Math.max(overlapUp, overlapDown));
+        }
+        return maxOverlap;
+    }
+}
